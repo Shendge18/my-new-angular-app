af2002da6882d47d566df6a411fff49214f387e4
Shendge18 <82372567+Shendge18@users.noreply.github.com>
2026-05-04 01:53:46 +0530

    Add task component and integrate user selection functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,30 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent } from "./header/header";
 import { User } from "./user/user";
+import { Task } from './task/task';
 import { DUMMY_USERS } from './dummy-users';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, User],
+  imports: [HeaderComponent, User, Task],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-new-angular-app');
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string){
-    console.log('Selected user with id:', id);
+    this.selectedUserId = id;
   }
+
+   
 }
