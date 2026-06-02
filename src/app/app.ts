136c9a50e48dc559226661d033d388d05e983507
Shendge18 <82372567+Shendge18@users.noreply.github.com>
2026-06-03 01:22:41 +0530

    Refactor user and task components for improved functionality and structure; remove unused files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,25 @@
 import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent } from "./header/header";
-import { User } from "./user/user";
-import { Task } from './task/task';
+import { UserComponent } from "./user/user";
+import { Tasks } from './tasks/tasks';
 import { DUMMY_USERS } from './dummy-users';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, User, Task],
+  imports: [HeaderComponent, UserComponent, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-new-angular-app');
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?:string;
 
   get selectedUser(){
-    return this.users.find(user => user.id === this.selectedUserId)!;
+    return this.users.find(user => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string){
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,15 +1,25 @@
 import { Component,computed, Input, Output, EventEmitter} from '@angular/core';
 
+// type User = {
+//     id: string;
+//     name: string;
+//     avatar: string
+//   }
+
+interface User{
+    id: string;
+    name: string;
+    avatar: string
+}
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+export class UserComponent {
+
+  @Input({required: true}) user!: User;
 
    @Output() select = new EventEmitter<string>();
 
@@ -21,10 +31,10 @@ export class User {
   // });
 
    get imagePath(){
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
    }
  
   onSelectuser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
