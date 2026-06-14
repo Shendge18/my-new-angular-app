c874db5cc3ea91b0a3aacac22795375135615b9c
Shendge18 <82372567+Shendge18@users.noreply.github.com>
2026-06-14 23:47:38 +0530

    Refactor task management: integrate TasksService for task handling, update NewTask component to use userId, and modify task list rendering

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,6 +1,7 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component,Input, Output, EventEmitter, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type newTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,23 +12,26 @@ import { type newTaskData } from '../task/task.model';
 
 
 export class NewTask {
-
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter< newTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
 
   enteredTitle = '';
   enteredSummary = '';
   enteredDueDate = '';
 
+  private tasksService = inject(TasksService);
+
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit(){
-    this.add.emit({ 
-      title: this.enteredTitle, 
-      summary: this.enteredSummary, 
-      dueDate: this.enteredDueDate   
-    });
+    this.tasksService.addTask({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDueDate
+    },this.userId)
+    
+    this.close.emit();
   }
 }
